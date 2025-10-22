dcf6dcd0cb64372476f0ec8aec2815a818f6f602
devolexa <starovatovasasha@gmail.com>
2025-10-22 09:51:31 +0300

    task four

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const loginform = document.querySelector("form");
+loginform.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.elements.email;
+    const password = form.elements.password;
+    if (email.value.trim() === "" || password.value.trim() === "") {
+        return alert("All form fields must be filled in");        
+    };
+    const data = {
+    email: email.value.trim(),
+    password: password.value.trim()
+    }
+    console.log(data);
+    form.reset()
+}
 
