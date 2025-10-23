66f4894f332b5e806ec1df3ec6f76bd9522a79be
devolexa <starovatovasasha@gmail.com>
2025-10-23 10:10:58 +0300

    task five

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.body;
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+btn.addEventListener("click", e => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  span.textContent = color;
+})
